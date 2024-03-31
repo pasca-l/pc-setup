fee6a5ac465190fd69a2a145ad4cb065098c6257
pasca-l <shion.yamadate@keio.jp>
2024-03-31 23:56:50 +0900

    added script file

---
+++ mac/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# add Homebrew to PATH, for zsh
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/sy/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# install applications via Homebrew
+# using Brewfile, created from `brew bundle dump`
+brew bundle --file=./Brewfile
