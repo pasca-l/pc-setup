dd66b30dfa8bd517a3607c8516f602a2e23b5ca0
pasca-l <shion.yamadate@keio.jp>
2024-08-24 13:17:54 +0900

    updated setup files

--- mac/setup.sh
+++ mac/setup.sh
@@ -3,12 +3,16 @@
 # install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # add Homebrew to PATH, for zsh
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/sy/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install applications via Homebrew
 # using Brewfile, created from `brew bundle dump`
 brew bundle --file=./Brewfile
+# add path for nodebrew
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> $HOME/.zshrc
+# load settings
+source $HOME/.zshrc
 
 # -- optional --
 # copy Latexmk to home directory
